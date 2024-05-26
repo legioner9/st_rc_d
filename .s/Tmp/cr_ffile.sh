d881c960639c8eb9f626e0f9d56609d392b813ff
[alt]legioner9 <legioner9@inbox.ru>
2024-05-26 17:55:58 +0700

    <> ##

---
+++ .s/Tmp/cr_ffile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for item in $(seq 1 1000); do
+    echo -e "casdcawcdwc
+    cdsacsdc
+    grtegrtg
+    hytrhyhryuh
+    " > /home/st/REPOBARE/_repo/st_rc_d/.s/Tmp/cr_ff/f1000
+
+done
