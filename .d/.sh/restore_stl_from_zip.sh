0ea8163add33ee879286bac25e2a14dd4626b848
[alt]legioner9 <legioner9@inbox.ru>
2024-10-09 10:50:52 +0700

    <> ##

---
+++ .d/.sh/restore_stl_from_zip.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+echo -e "${CYAN}--- restore_stl_from_zip_rnd7_747dad6() $* in file://${ST_RC_D_PATH}/.d/.sh/restore_stl_from_zip.sh ---${NORMAL}" #started functions
+
+restore_stl_from_zip_rnd7_747dad6() {
+
+    # gig from file://${ST_RC_D_PATH}/.d/.arb/stl0.arb/ufl_stl0.ram/.grot/ufl_stl0.sh
+
+    local FNN=${FUNCNAME[0]}
+    local ARGS=("$@")
+    local NARGS=$#
+    local PPWD=$PWD
+    local path_file="${ST_RC_D_PATH}/.d/.sh/restore_stl_from_zip.sh"
+    local path_dir="$(dirname "$path_file")"
+
+    # echo -e "${CYAN}--- $FNN() $* in file://${path_file}---${NORMAL}" #started functions
+
+    cd ${path_dir} || {
+        # hint="\$1: \$2: "
+        _st_exit "in fs= file://$path_file , line=${LINENO}, ${FNN}() : NOT_DIR : 'file://${path_dir}' : ${hint} : return 1"
+        return 1
+    }
+
+    if [ "-h" == "$1" ]; then
+        echo -e "${CYAN} ${FNN}() help: 
+MAIN: 
+TAGS: @
+ARGS: \$1
+EXAM: 
+EXEC: 
+CNTL: 
+    _go  : _edit ${path_file}
+RETURN: ( result>stdout, return 0 | data | change to ptr |  fs_structure | ...)
+ERROR: ( return 1 | ... )
+    ${FNN} 
+${NORMAL}"
+    fi
+
+    if [[ "_go" == "$1" ]]; then
+        _edit "${path_file}"
+
+        cd $PPWD
+        return 0
+    fi
+
+    # hint="\$1: \$2: "
+    # if _isn_from ${NARGS} less more "in fs= file://${path_file} , line=${LINENO}, ${FNN}() : ERR_AMOUNT_ARGS entered :'${NARGS}' args : ${hint} : return 1"; then
+    #     return 1
+    # fi
+
+    # local _ARGS_=("$@")
+    # _parr3e _ARGS_
+
+    #{{ptr_path}}
+
+    #! START BODY FN ---------------------------------------
+
+    echo "START BODY FN : ${FNN}() $*"
+
+    #{{body_fn}}
+
+    local dt=
+    local st=
+    local post_date=
+
+    dt=$(date +%d%m%Y%H%M%S)
+    st=$(date +%s)
+    post_date="${st}_${dt}"
+
+    [ -f ${HOME}/.d.zip ] && {
+        mv ${HOME}/.d.zip ${HOME}/.d.zip.${post_date}
+    }
+
+    cp ${ST_RC_D_PATH}/.d/.zip/.d.zip ${HOME}
+    cd ${HOME}
+    unzip .d.zip
+
+    #! END BODY FN ---------------------------------------
+
+    cd $PPWD
+    return 0
+
+}
+
+restore_stl_from_zip_rnd7_747dad6 "$@"
