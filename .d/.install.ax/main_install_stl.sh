23346dd78aaf0f7be535faa5614aaf3bc3b5bebf
[alt]legioner9 <legioner9@inbox.ru>
2024-10-10 13:11:34 +0700

    <> ##

--- .d/.install.ax/main_install_stl.sh
+++ .d/.install.ax/main_install_stl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo -e "${CYAN}--- main_install_stl_rnd7_21b4629() $* in file://${ST_RC_D_PATH}/.d/install.ax/main_install_stl.sh ---${NORMAL}" #started functions
+echo -e "${CYAN}--- main_install_rnd7_55011d8() $* in file://${HOME}/.install.sh/main_install.sh ---${NORMAL}" #started functions
 
-main_install_stl_rnd7_21b4629() {
+main_install_rnd7_55011d8() {
 
     # gig from file://${ST_RC_D_PATH}/.d/.arb/stl0.arb/ufl_stl0.ram/.grot/ufl_stl0.sh
 
@@ -10,14 +10,19 @@ main_install_stl_rnd7_21b4629() {
     local ARGS=("$@")
     local NARGS=$#
     local PPWD=$PWD
-    local path_file="${ST_RC_D_PATH}/.d/install.ax/main_install_stl.sh"
+    local path_file="${HOME}/.install.sh/main_install.sh"
     local path_dir="$(dirname "$path_file")"
+
+    local norm="\033[0m"
+    local fon_1="\033[7m \033[1m \033[31m \033[40m"
+    # \033[0;31m${_W_CONST}\033[0;32m${arr_lib[*]} \033[0;35m\u@\H \w$(__git_ps1 " (%s)") \$\033[0m
+    PS1="\033[1m\033[40m\033[34m\u\033[1m\033[40m\033[35m@\H \033[1m\033[40m\033[36m\w\033[1m\033[40m\033[35m \$\033[0m"
 
     # echo -e "${CYAN}--- $FNN() $* in file://${path_file}---${NORMAL}" #started functions
 
     cd ${path_dir} || {
         # hint="\$1: \$2: "
-        _st_exit "in fs= file://$path_file , line=${LINENO}, ${FNN}() : NOT_DIR : 'file://${path_dir}' : ${hint} : return 1"
+        echo -e "${fon_1}in fs= file://${path_file} , line=${LINENO}, ${FNN}() : NOT_DIR : 'file://${path_dir}' : ${hint} : return 1${norm}"
         return 1
     }
 
@@ -56,6 +61,7 @@ ${NORMAL}"
     #! START BODY FN ---------------------------------------
 
     echo "START BODY FN : ${FNN}() $*"
+    
 
     #{{body_fn}}
 
@@ -66,4 +72,4 @@ ${NORMAL}"
 
 }
 
-main_install_stl_rnd7_21b4629 "$@"
+main_install_rnd7_55011d8 "$@"
