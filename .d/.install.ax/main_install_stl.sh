f380f9ccb0fa596ca50ae74ba520e4e6ab32e527
[alt]legioner9 <legioner9@inbox.ru>
2024-10-12 01:40:27 +0700

    <> ##

--- .d/.install.ax/main_install_stl.sh
+++ .d/.install.ax/main_install_stl.sh
@@ -14,54 +14,139 @@ main_install_rnd7_55011d8() {
     local path_dir="$(dirname "$path_file")"
 
     local norm="\033[0m"
-    local fon_1="\033[7m \033[1m \033[31m \033[40m"
+    local rvrc="\033[7m"
+
+    local font_black="\033[30m"
+    local font_red="\033[31m"
+    local font_green="\033[32m"
+    local font_yellow="\033[33m"
+    local font_blue="\033[34m"
+    local font_purple="\033[35m"
+    local font_cyan="\033[36m"
+    local font_white="\033[37m"
+
+    local grnd_black="\033[1m\033[40m\033[30m"
+    local grnd_red="\033[1m\033[40m\033[31m"
+    local grnd_green="\033[1m\033[40m\033[32m"
+    local grnd_yellow="\033[1m\033[40m\033[33m"
+    local grnd_blue="\033[1m\033[40m\033[34m"
+    local grnd_purple="\033[1m\033[40m\033[35m"
+    local grnd_cyan="\033[1m\033[40m\033[36m"
+    local grnd_white="\033[1m\033[40m\033[37m"
+
+    # echo -e "${grnd_black} grnd_black ${norm}"
+    # echo -e "${grnd_red} grnd_red ${norm}"
+    # echo -e "${grnd_green} grnd_green ${norm}"
+    # echo -e "${grnd_yellow} grnd_yellow ${norm}"
+    # echo -e "${grnd_blue} grnd_blue ${norm}"
+    # echo -e "${grnd_purple} grnd_purple ${norm}"
+    # echo -e "${grnd_cyan} grnd_cyan ${norm}"
+    # echo -e "${grnd_white} grnd_white ${norm}"
+
     # \033[0;31m${_W_CONST}\033[0;32m${arr_lib[*]} \033[0;35m\u@\H \w$(__git_ps1 " (%s)") \$\033[0m
-    PS1="\033[1m\033[40m\033[34m\u\033[1m\033[40m\033[35m@\H \033[1m\033[40m\033[36m\w\033[1m\033[40m\033[35m \$\033[0m"
+    PS1="${grnd_cyan}\u${grnd_black}@\H${grnd_green}\w${norm}"
 
     # echo -e "${CYAN}--- $FNN() $* in file://${path_file}---${NORMAL}" #started functions
 
-    cd ${path_dir} || {
-        # hint="\$1: \$2: "
-        echo -e "${fon_1}in fs= file://${path_file} , line=${LINENO}, ${FNN}() : NOT_DIR : 'file://${path_dir}' : ${hint} : return 1${norm}"
-        return 1
-    }
-
-    if [ "-h" == "$1" ]; then
-        echo -e "${CYAN} ${FNN}() help: 
-MAIN: 
-TAGS: @
-ARGS: \$1
-EXAM: 
-EXEC: 
-CNTL: 
-    _go  : _edit ${path_file}
-RETURN: ( result>stdout, return 0 | data | change to ptr |  fs_structure | ...)
-ERROR: ( return 1 | ... )
-    ${FNN} 
-${NORMAL}"
-    fi
-
-    if [[ "_go" == "$1" ]]; then
-        _edit "${path_file}"
-
-        cd $PPWD
-        return 0
-    fi
-
-    # hint="\$1: \$2: "
-    # if _isn_from ${NARGS} less more "in fs= file://${path_file} , line=${LINENO}, ${FNN}() : ERR_AMOUNT_ARGS entered :'${NARGS}' args : ${hint} : return 1"; then
+    # cd ${path_dir} || {
+    #     # hint="\$1: \$2: "
+    #     echo -e "${font_err}in fs= file://${path_file} , line=${LINENO}, ${FNN}() : NOT_DIR : 'file://${path_dir}' : ${hint} : return 1${norm}" >&2
     #     return 1
-    # fi
-
-    # local _ARGS_=("$@")
-    # _parr3e _ARGS_
-
-    #{{ptr_path}}
+    # }
 
     #! START BODY FN ---------------------------------------
 
+
+
     echo "START BODY FN : ${FNN}() $*"
+
+    erro_rnd7_55011d8(){
+        echo -e "${grnd_red}$1${norm}" >&2
+    }
+
+    varn_rnd7_55011d8(){
+        echo -e "${grnd_yellow}$1${norm}"
+    }
+
+    info_rnd7_55011d8(){
+        echo -e "${grnd_green}$1${norm}"
+    }
+
+    is_yes_rnd7_55011d8(){    
+        # hint string
+        local yes
+        echo -e "${grnd_green}$1${norm}"
+        read -r -p "only 'y' accept the offer (y|) " yes
+        echo -e "${grnd_green}You enter : ${yes}${norm}"
+        if [ "${yes:-no}" == "y" ]; then
+            cd $PPWD
+            return 0
+        else
+            cd $PPWD
+            return 1
+        fi
+    }
+ 
+
+
+    # echo -e "${fon_1} warn string ${norm}" >&2
+
+    varn_rnd7_55011d8 "Default parameters install: 
+    ${HOME}/STL - dir with .d.zip STL and functions STL0
+    ${HOME}/STL_DATA - dir with user data for functions STL0"
+
+    is_yes_rnd7_55011d8 "DO? : Continue with that Default parameters" || {
+        info_rnd7_55011d8 "reject install : return 0"
+        cd $PPWD
+        return 0
+    }
+
+    mkdir_rnd7_55011d8(){
+
+    if [ -d "$1" ];then 
+        erro_rnd7_55011d8 "DIR_EXIST : ' file://$1 '"
+        is_yes_rnd7_55011d8 "DO? : remove ' file://$1 ' dir?" && {
+
+            varn_rnd7_55011d8 "rm -r $1"                        
+            rm -r "$1"
+
+            varn_rnd7_55011d8 "mkdir $1"
+            mkdir "$1" || {
+                erro_rnd7_55011d8 "FAIL_EXEC : 'mkdir $1' return 1"
+                cd $PPWD
+                return 1
+            }
+
+        }
     
+    else 
+        varn_rnd7_55011d8 "mkdir $1"
+        mkdir "$1" || {
+            erro_rnd7_55011d8 "FAIL_EXEC : 'mkdir $1' return 1"
+            cd $PPWD
+            return 1
+        }
+    fi
+
+    }
+
+    mkdir_rnd7_55011d8 ${HOME}/STL
+    mkdir_rnd7_55011d8 ${HOME}/STL_DATA
+
+    command -v wget >/dev/null || {
+        err_rnd7_55011d8 "util 'wget' not find : return 1" 
+        cd $PPWD
+        return 1
+    }
+
+    varn_rnd7_55011d8 "Default parameters download from github.com @legioner9"
+
+    is_yes_rnd7_55011d8 "DO? : Continue with that Default parameters download" || {
+        info_rnd7_55011d8 "reject install : return 0"
+        cd $PPWD
+        return 0
+    }
+
 
     #{{body_fn}}
 
