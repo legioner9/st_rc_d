510da4d680a862c98800c347fe246b02a7b77eb1
[alt]legioner9 <legioner9@inbox.ru>
2024-10-12 03:20:15 +0700

    <> ##

--- .d/.install.ax/main_install_stl.sh
+++ .d/.install.ax/main_install_stl.sh
@@ -44,7 +44,7 @@ main_install_rnd7_55011d8() {
     # echo -e "${grnd_white} grnd_white ${norm}"
 
     # \033[0;31m${_W_CONST}\033[0;32m${arr_lib[*]} \033[0;35m\u@\H \w$(__git_ps1 " (%s)") \$\033[0m
-    PS1="${grnd_cyan}\u${grnd_black}@\H${grnd_green}\w${norm}"
+    PS1="${grnd_cyan}\u${grnd_black} @\H${grnd_green} \w $ ${norm}"
 
     # echo -e "${CYAN}--- $FNN() $* in file://${path_file}---${NORMAL}" #started functions
 
@@ -75,8 +75,8 @@ main_install_rnd7_55011d8() {
     is_yes_rnd7_55011d8(){    
         # hint string
         local yes
-        echo -e "${grnd_green}$1${norm}"
-        read -r -p "only 'y' accept the offer (y|) " yes
+        echo -e "${grnd_green}$1 : only 'y' accept the offer${norm}"
+        read -r -p " (y|) " yes
         echo -e "${grnd_green}You enter : ${yes}${norm}"
         if [ "${yes:-no}" == "y" ]; then
             cd $PPWD
@@ -90,6 +90,12 @@ main_install_rnd7_55011d8() {
 
 
     # echo -e "${fon_1} warn string ${norm}" >&2
+
+    command -v wget >/dev/null || {
+        err_rnd7_55011d8 "util 'wget' not find : return 1" 
+        cd $PPWD
+        return 1
+    }
 
     varn_rnd7_55011d8 "Default parameters install: 
     ${HOME}/STL - dir with .d.zip STL and functions STL0
@@ -107,8 +113,8 @@ main_install_rnd7_55011d8() {
         erro_rnd7_55011d8 "DIR_EXIST : ' file://$1 '"
         is_yes_rnd7_55011d8 "DO? : remove ' file://$1 ' dir?" && {
 
-            varn_rnd7_55011d8 "rm -r $1"                        
-            rm -r "$1"
+            varn_rnd7_55011d8 "rm -rf $1"                        
+            rm -rf "$1"
 
             varn_rnd7_55011d8 "mkdir $1"
             mkdir "$1" || {
@@ -133,20 +139,64 @@ main_install_rnd7_55011d8() {
     mkdir_rnd7_55011d8 ${HOME}/STL
     mkdir_rnd7_55011d8 ${HOME}/STL_DATA
 
-    command -v wget >/dev/null || {
-        err_rnd7_55011d8 "util 'wget' not find : return 1" 
-        cd $PPWD
-        return 1
-    }
-
-    varn_rnd7_55011d8 "Default parameters download from github.com @legioner9"
+    varn_rnd7_55011d8 "Default parameters download from github.com @legioner9 (use wget)"
 
     is_yes_rnd7_55011d8 "DO? : Continue with that Default parameters download" || {
+        
+        is_yes_rnd7_55011d8 "Did you upload STL and STL_DATA yourself?" || {
+            info_rnd7_55011d8 "reject install : return 0"
+            cd $PPWD
+            return 0
+        }
+    }
+
+    # wget https://github.com/legioner9/st_rc_d/archive/refs/heads/master.zip
+    # wget https://gitflic.ru/project/legioner9/st_rc_d/file/downloadAll?branch=master
+
+
+    if is_yes_rnd7_55011d8 "DO? wget https://github.com/legioner9/st_rc_d/archive/refs/heads/master.zip in ${HOME}/STL" ; then
+
+        wget https://github.com/legioner9/st_rc_d/archive/refs/heads/master.zip -O ${HOME}/STL/master.zip|| {
+            erro_rnd7_55011d8 "FAIL_EXEC : 'wget https://github.com/legioner9/st_rc_d/archive/refs/heads/master.zip -O ${HOME}/STL/master.zip' return 1"
+            cd $PPWD
+            return 1
+        }
+
+        ${HOME}/STL
+        unzip 
+
+
+
+    else 
         info_rnd7_55011d8 "reject install : return 0"
         cd $PPWD
         return 0
-    }
+    fi
 
+    # https://github.com/legioner9/st_rc_d_data/archive/refs/heads/master.zip
+    # https://gitflic.ru/project/st_rc_d_data/sta/file/downloadAll?branch=master
+
+    if is_yes_rnd7_55011d8 "DO? wget https://github.com/legioner9/st_rc_d_data/archive/refs/heads/master.zip in ${HOME}/STL_DATA " ; then
+        if [ -f ${HOME}/STL_DATA/master.zip ] ; then
+            is_yes_rnd7_55011d8 "FILE_EXIST ${HOME}/STL_DATA/master.zip rewget ? " && {
+                rm -f ${HOME}/STL_DATA/master.zip
+                wget https://github.com/legioner9/st_rc_d_data/archive/refs/heads/master.zip -O ${HOME}/STL_DATA/master.zip || {
+                    erro_rnd7_55011d8 "FAIL_EXEC : 'wget https://github.com/legioner9/st_rc_d_data/archive/refs/heads/master.zip -O ${HOME}/STL_DATA/master.zip' return 1"
+                    cd $PPWD
+                    return 1
+            }
+        }
+        else
+            wget https://github.com/legioner9/st_rc_d_data/archive/refs/heads/master.zip -O ${HOME}/STL_DATA/master.zip || {
+            erro_rnd7_55011d8 "FAIL_EXEC : 'wget https://github.com/legioner9/st_rc_d_data/archive/refs/heads/master.zip -O ${HOME}/STL_DATA/master.zip' return 1"
+            cd $PPWD
+            return 1
+        fi
+    else 
+        info_rnd7_55011d8 "reject install : return 0"
+        cd $PPWD
+        return 0
+    fi
 
     #{{body_fn}}
 
